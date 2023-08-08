0bfa5c2a76de43965b9c7f69d401e27e3533e5c0
radiatom <arssenmozol@gmail.com>
2023-08-08 16:17:23 +0300

    create page github

--- src/redux/redux.js
+++ src/redux/redux.js
@@ -10,7 +10,7 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
 const persistConfig = {
-    key: '45684616df5431247hjfkfhjhf',
+    key: '45684616df543124ghf7hjfkfhjhf',
     storage,
 }// для local storage
 
