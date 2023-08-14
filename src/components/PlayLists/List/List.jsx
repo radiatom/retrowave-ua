0319d86773b949d76c7ce17a49dc148f8137f5b6
radiatom <arssenmozol@gmail.com>
2023-08-14 12:51:53 +0300

    added logic animation btn

--- src/components/PlayLists/List/List.jsx
+++ src/components/PlayLists/List/List.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./List.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { namesPlayListsSelector } from "../../../selectorApp";
@@ -6,19 +6,23 @@ import Track from "./Track/Track";
 const List = ({ position, list }) => {
     const namesPlayLists = useSelector(namesPlayListsSelector);
     const dispatch = useDispatch();
-
-    const openList =(name)=>{
-      dispatch({ type: `crate${name}List` });
+      const [openListName,setOpenlistName]=useState('')
+    const openList = (name) => {
+        dispatch({ type: `crate${name}List` });
         dispatch({ type: "setList", position, typeList: name });
-    }//відкрити плейлист за назвою назва має бути з великої літери
+        setOpenlistName(name)
+    }; //відкрити плейлист за назвою назва має бути з великої літери
 
     return (
         <div className="list">
             <div className="list__buttons">
+                <button className="list__btn">
+                    add
+                </button>
                 {namesPlayLists.map((item) => {
                     return (
                         <button
-                            className="list__btn"
+                            className={openListName===item?"list__btn open":"list__btn"}
                             onClick={() => openList(item)}
                         >
                             {item}
