fb8749580bdf821a63ddb0def044a9a7cf22e5ff
radiatom <arssenmozol@gmail.com>
2023-08-14 16:50:03 +0300

    added addIco for track

--- src/components/PlayLists/List/Track/Track.jsx
+++ src/components/PlayLists/List/Track/Track.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import './Track.scss'
 import Rating from '../../../Rating/Rating';
 import { newTime } from '../../../../function';
+import { useState } from 'react';
+import addIco from'./../../../../img/icons/add.svg'
 const Track = ({title,index,rating,duration,id}) => {
+      const [active,setActive]=useState(false)
       return (
             <div className='track'>
                   <div className='track__position'>{index+1}</div>
                   <div className='track__title'>{title}</div>
                   <div className='track__rating'><Rating rating={rating} id={id}/> </div>
                   <div className='track__duration'>{newTime(Math.round(duration/1000))}</div>
+                  <img className={active?'track__addIco active':'track__addIco'} onClick={()=>setActive(!active)} src={addIco} alt="addIco" />
             </div>
       );
 }
