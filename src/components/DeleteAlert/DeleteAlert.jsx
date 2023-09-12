0c132d65a18c221ea4a0d9c25b848a71efc62e0b
radiatom <arssenmozol@gmail.com>
2023-09-12 12:38:17 +0300

    fisrt commit

---
+++ src/components/DeleteAlert/DeleteAlert.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './DeleteAlert.scss'
+const DeleteAlert = ({setYesOrNo}) => {
+    return (
+        <div className='deleteAlert'>
+            <button onClick={()=>setYesOrNo(true)}>Yes</button><button onClick={()=>setYesOrNo(false)}>No</button>
+        </div>
+    );
+}
+
+export default DeleteAlert;
