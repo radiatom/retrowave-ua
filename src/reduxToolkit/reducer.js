34c846e14e3738af7990dcda7f198563a44e6be7
radiatom <arssenmozol@gmail.com>
2023-09-23 12:33:37 +0300

    changed url

--- src/reduxToolkit/reducer.js
+++ src/reduxToolkit/reducer.js
@@ -257,10 +257,10 @@ export const addMusics = () => async (dispatch) => {
         // streamUrl: "/retrowave-ua/assets"+`${track.streamUrl}`, //для сервера
         // artworkUrl:`${process.env.PUBLIC_URL}${track.artworkUrl}`,//для сервера
         // streamUrl:`${process.env.PUBLIC_URL}${track.streamUrl}`//для сервера
-        // artworkUrl:`${track.artworkUrl}`,//працює лише локально але не на хостінгу
-        // streamUrl:`${track.streamUrl}`//працює лише локально але не на хостінгу
-        artworkUrl: new URL(`${track.artworkUrl}`, import.meta.url).href, //для сервера
-        streamUrl: new URL(`${track.streamUrl}`, import.meta.url).href, //для сервера
+        // artworkUrl:`${track.artworkUrl}`,//працює лише локально але не на хостінгу(аудіо та картинки в publick)
+        // streamUrl:`${track.streamUrl}`//працює лише локально але не на хостінгу(аудіо та картинки в publick)
+        artworkUrl: new URL(`${track.artworkUrl}`, import.meta.url).href, //працює лише локально але не на хостінгу(аудіо та картинки в publick). коли аудіо та картинки в src локально не працює
+        streamUrl: new URL(`${track.streamUrl}`, import.meta.url).href, //працює лише локально але не на хостінгу(аудіо та картинки в publick). коли аудіо та картинки в src локально не працює
         // artworkUrl:`${import.meta.env.VITE_PUBLIC_URL+"/assets"+track.artworkUrl}`,//для сервера
         // streamUrl:`${import.meta.env.VITE_PUBLIC_URL+"/assets"+track.streamUrl}`//для сервера
         // artworkUrl:"https://retrowave.ru"+track.artworkUrl,//для api
