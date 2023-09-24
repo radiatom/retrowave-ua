a6fc4e4cefb695e37df964bb6b2ff1941490b210
radiatom <arssenmozol@gmail.com>
2023-09-24 13:07:39 +0300

    refactored code

--- src/function.ts
+++ src/function.ts
@@ -1,3 +1,5 @@
+import { dataType } from "./server";
+
 export const newTime = (second:number):string => {
     const minutes = Math.floor(second / 60);
     const remainingSeconds = second % 60;
@@ -15,20 +17,18 @@ export const capitalizeFirstLetter = (str:string):string => {
     return str[0].toUpperCase() + str.slice(1);
 }; // робить першу літеру рядка великою
 
-export const lowerFirstLetter = (str:string):string => {
-    if (str.length === 0) {
-        return str;
-    }
-    return str[0].toLowerCase() + str.slice(1);
-}; // робить першу літеру рядка низькою
-//@ts-ignore
-export const checkNameInArray = (name:string, array):boolean => {
-    return array.includes(name);
-}; //функція яка перевіряє наявність певної назви в масиві.return  true or folse
-//@ts-ignore
-export const updateValueById = (array, idToFind, newValue) => {
-//@ts-ignore
+// export const lowerFirstLetter = (str:string):string => {
+//     if (str.length === 0) {
+//         return str;
+//     }
+//     return str[0].toLowerCase() + str.slice(1);
+// }; // робить першу літеру рядка низькою
 
+// export const checkNameInArray = (name, array) => {
+//     return array.includes(name);
+// }; //функція яка перевіряє наявність певної назви в масиві.return  true or folse
+
+export const updateValueById = (array:dataType, idToFind:string, newValue:number) => {
     const updatedArray = array.map((item) => {
         if (item.id === idToFind) {
             // Копіюємо об'єкт і змінюємо значення
@@ -38,6 +38,7 @@ export const updateValueById = (array, idToFind, newValue) => {
     });
     return updatedArray;
 };//оновлення рейтинга
+
 export const containsLatinAndDigits = (inputString:string):boolean => {
     const pattern = /^[a-zA-Z0-9]+$/;
     return pattern.test(inputString);
