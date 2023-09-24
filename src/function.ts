00b9fa98e8322ade136a739b127962dea1b3e762
radiatom <arssenmozol@gmail.com>
2023-09-24 12:44:43 +0300

    add ts

--- src/function.ts
+++ src/function.ts
@@ -1,31 +1,34 @@
-export const newTime = (second) => {
+export const newTime = (second:number):string => {
     const minutes = Math.floor(second / 60);
     const remainingSeconds = second % 60;
     return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
 }; //функція для перетворення секунди в хвилини
 
-export const newTimeForBoombox = (second) => {
+export const newTimeForBoombox = (second:number):string => {
     return second.toString().padStart(3, "0");
 }; //функція для перетворення секунди в значення 000
 
-export const capitalizeFirstLetter = (str) => {
+export const capitalizeFirstLetter = (str:string):string => {
     if (str.length === 0) {
         return str;
     }
     return str[0].toUpperCase() + str.slice(1);
 }; // робить першу літеру рядка великою
 
-export const lowerFirstLetter = (str) => {
+export const lowerFirstLetter = (str:string):string => {
     if (str.length === 0) {
         return str;
     }
     return str[0].toLowerCase() + str.slice(1);
 }; // робить першу літеру рядка низькою
-
-export const checkNameInArray = (name, array) => {
+//@ts-ignore
+export const checkNameInArray = (name:string, array):boolean => {
     return array.includes(name);
 }; //функція яка перевіряє наявність певної назви в масиві.return  true or folse
+//@ts-ignore
 export const updateValueById = (array, idToFind, newValue) => {
+//@ts-ignore
+
     const updatedArray = array.map((item) => {
         if (item.id === idToFind) {
             // Копіюємо об'єкт і змінюємо значення
@@ -34,13 +37,13 @@ export const updateValueById = (array, idToFind, newValue) => {
         return item;
     });
     return updatedArray;
-};
-export const containsLatinAndDigits = (inputString) => {
+};//оновлення рейтинга
+export const containsLatinAndDigits = (inputString:string):boolean => {
     const pattern = /^[a-zA-Z0-9]+$/;
     return pattern.test(inputString);
-};
+};//перевірка на наявність криличний букв
 
-export const timeToMilliseconds = (timeString) => {
+export const timeToMilliseconds = (timeString:string):number => {
     const [minutes, seconds] = timeString.split(":");
     const totalMilliseconds = parseInt(minutes) * 60 * 1000 + parseInt(seconds) * 1000;
     return totalMilliseconds;
