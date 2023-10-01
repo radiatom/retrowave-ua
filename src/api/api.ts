468d1ac16bc7af775a1ca3089524f83b36f38cfe
radiatom <arssenmozol@gmail.com>
2023-10-01 18:01:19 +0300

    test

--- src/api/api.ts
+++ src/api/api.ts
@@ -7,14 +7,14 @@ const BASE_URL = 'https://retrowave.ru/api/v1/tracks?cursor=1&limit=199'
 export const allApi = {
     getAddMusics: () => {
         // return data1Dowland //нові майбутні
-        // return new Promise(resolve => {
-        //     setTimeout(() => {
-        //       resolve(getData());//мій сервер
-        //     }, 300);
-        //   });
-        return axios.get(`${BASE_URL}`)
-            .then((response) => {
-                return response.data.body.tracks;
-            });
+        return new Promise(resolve => {
+            setTimeout(() => {
+              resolve(getData());//мій сервер
+            }, 300);
+          });
+        // return axios.get(`${BASE_URL}`)
+        //     .then((response) => {
+        //         return response.data.body.tracks;
+        //     });
     },
 };
